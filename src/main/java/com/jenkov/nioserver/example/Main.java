7ff8ac42805145f96610200c315fec310d15f24d
yudnkuku <yudnkuku@163.com>
2018-11-16 16:20:33 +0800

    Main Method

--- src/main/java/com/jenkov/nioserver/example/Main.java
+++ src/main/java/com/jenkov/nioserver/example/Main.java
@@ -1,11 +1,11 @@
 package com.jenkov.nioserver.example;
 
-import com.jenkov.nioserver.*;
+import com.jenkov.nioserver.IMessageProcessor;
+import com.jenkov.nioserver.Message;
+import com.jenkov.nioserver.Server;
 import com.jenkov.nioserver.http.HttpMessageReaderFactory;
 
 import java.io.IOException;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 
 /**
  * Created by jjenkov on 19-10-2015.
@@ -22,6 +22,8 @@ public class Main {
 
         byte[] httpResponseBytes = httpResponse.getBytes("UTF-8");
 
+        //消息处理器实现，将响应封装成message放入writeProxy的响应消息队列中
+        //这个处理器实现很简单，没有分析请求的内容，只拿了请求socket id，写死响应
         IMessageProcessor messageProcessor = (request, writeProxy) -> {
             System.out.println("Message Received from socket: " + request.socketId);
 
